daf105a46b780203cf89c514818a39ac62241f63
samuelawijaya <samuel08wijaya@gmail.com>
2026-03-16 19:18:00 -0400

    Sam I2C

---
+++ Core/Src/main.c
@@ -0,0 +1,595 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+#include "ssd1306.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c2;
+
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
+
+I2C_HandleTypeDef hi2c2;
+
+UART_HandleTypeDef huart3;
+
+PCD_HandleTypeDef hpcd_USB_OTG_FS;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_USART3_UART_Init(void);
+static void MX_USB_OTG_FS_PCD_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_I2C2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+#define DIR_PIN_X GPIO_PIN_15
+#define DIR_PORT_X GPIOF
+#define STEP_PIN_X GPIO_PIN_13
+#define STEP_PORT_X GPIOE
+
+#define DIR_PIN_Y GPIO_PIN_12
+#define DIR_PORT_Y GPIOF
+#define STEP_PIN_Y GPIO_PIN_15
+#define STEP_PORT_Y GPIOD
+
+// Function to set servo angle
+void Set_Servo_Angle(TIM_HandleTypeDef *htim, uint32_t channel, uint8_t angle)
+{
+    uint32_t pulse = 500 + (angle * (2500 - 500) / 180);
+    __HAL_TIM_SET_COMPARE(htim, channel, pulse);
+}
+
+void microDelay (uint16_t delay)
+{
+  __HAL_TIM_SET_COUNTER(&htim1, 0);
+  while (__HAL_TIM_GET_COUNTER(&htim1) < delay);
+}
+
+void step_X (int steps, uint8_t direction, uint16_t delay) {
+  HAL_GPIO_WritePin(DIR_PORT_X, DIR_PIN_X, direction ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  for(int x=0; x<steps; x++) {
+    HAL_GPIO_WritePin(STEP_PORT_X, STEP_PIN_X, GPIO_PIN_SET);
+    microDelay(10); // Fixed short pulse
+    HAL_GPIO_WritePin(STEP_PORT_X, STEP_PIN_X, GPIO_PIN_RESET);
+    microDelay(delay); // Speed control
+  }
+}
+
+void step_Y (int steps, uint8_t direction, uint16_t delay) {
+  HAL_GPIO_WritePin(DIR_PORT_Y, DIR_PIN_Y, direction ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  for(int x=0; x<steps; x++) {
+    HAL_GPIO_WritePin(STEP_PORT_Y, STEP_PIN_Y, GPIO_PIN_SET);
+    microDelay(10); // Fixed short pulse
+    HAL_GPIO_WritePin(STEP_PORT_Y, STEP_PIN_Y, GPIO_PIN_RESET);
+    microDelay(delay); // Speed control
+  }
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM1_Init();
+  MX_USART3_UART_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  MX_TIM2_Init();
+  MX_I2C2_Init();
+  /* USER CODE BEGIN 2 */
+  // Start the PWM signal generation
+  	 HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  // 1. Start the Timer hardware so microDelay works
+    HAL_TIM_Base_Start(&htim1);
+
+    // 2. Bring PD14 HIGH to wake up the A4988 driver
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+
+    // Give the driver a tiny moment to wake up
+    HAL_Delay(10);
+  /* USER CODE END 2 */
+
+    // I2C Init
+    SSD1306_Init();
+    SSD1306_Clear();
+    OLED_ShowProgress(0);
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    int y;
+    for(y=0; y<50; y=y+1) // 8 times
+    {
+      step_X(1, 0, 800); // 25 steps (45 degrees) CCV
+      step_Y(1, 0, 800);
+
+      OLED_ShowProgress((y + 1) * 100 / 50);
+
+      HAL_Delay(100);
+    }
+    // 2000 microsecond delay is much slower and has higher torque
+	step_X(200, 1, 2000);
+	step_Y(200, 1, 2000);
+	HAL_Delay(1000);
+
+// Sweep servo from 0 to 180 degrees
+	  for (uint8_t angle = 0; angle <= 180; angle += 10)
+	  {
+		  Set_Servo_Angle(&htim2, TIM_CHANNEL_1, angle);
+		  HAL_Delay(100);
+	  }
+
+	  // Sweep back from 180 to 0 degrees
+	  for (uint8_t angle = 180; angle > 0; angle -= 10)
+	  {
+		  Set_Servo_Angle(&htim2, TIM_CHANNEL_1, angle);
+		  HAL_Delay(100);
+	  }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  OLED_ShowProgress(0);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 168-1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 168-1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 20000-1;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
+  * @brief USB_OTG_FS Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USB_OTG_FS_PCD_Init(void)
+{
+
+  /* USER CODE BEGIN USB_OTG_FS_Init 0 */
+
+  /* USER CODE END USB_OTG_FS_Init 0 */
+
+  /* USER CODE BEGIN USB_OTG_FS_Init 1 */
+
+  /* USER CODE END USB_OTG_FS_Init 1 */
+  hpcd_USB_OTG_FS.Instance = USB_OTG_FS;
+  hpcd_USB_OTG_FS.Init.dev_endpoints = 6;
+  hpcd_USB_OTG_FS.Init.speed = PCD_SPEED_FULL;
+  hpcd_USB_OTG_FS.Init.dma_enable = DISABLE;
+  hpcd_USB_OTG_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
+  hpcd_USB_OTG_FS.Init.Sof_enable = ENABLE;
+  hpcd_USB_OTG_FS.Init.low_power_enable = DISABLE;
+  hpcd_USB_OTG_FS.Init.lpm_enable = DISABLE;
+  hpcd_USB_OTG_FS.Init.vbus_sensing_enable = ENABLE;
+  hpcd_USB_OTG_FS.Init.use_dedicated_ep1 = DISABLE;
+  if (HAL_PCD_Init(&hpcd_USB_OTG_FS) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USB_OTG_FS_Init 2 */
+
+  /* USER CODE END USB_OTG_FS_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9|GPIO_PIN_11|GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : USER_Btn_Pin */
+  GPIO_InitStruct.Pin = USER_Btn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA5 PA6 PA7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
+  GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PF12 PF13 PF14 PF15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PE9 PE11 PE13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_11|GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD14 PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : USB_PowerSwitchOn_Pin */
+  GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(USB_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : USB_OverCurrent_Pin */
+  GPIO_InitStruct.Pin = USB_OverCurrent_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USB_OverCurrent_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ Core/Src/ssd1306.c
@@ -0,0 +1,284 @@
+/**
+* original author:  Tilen Majerle<tilen@majerle.eu>
+* modification for STM32f10x: Alexander Lutsai<s.lyra@ya.ru>
+ ----------------------------------------------------------------------
+        Copyright (C) Alexander Lutsai, 2016
+  Copyright (C) Tilen Majerle, 2015
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ----------------------------------------------------------------------
+*/
+#include "ssd1306.h"
+extern I2C_HandleTypeDef hi2c2;
+/* SSD1306 data buffer. This is the buffer you must write to for setting pixel
+ * values. */
+static uint8_t SSD1306_Buffer[SSD1306_WIDTH * SSD1306_HEIGHT / 8];
+/* The set of registers to write to when initializing the SSD1306 screen. */
+static uint8_t SSD1306_Init_Config[] = {
+    0xAE, 0x20, 0x10, 0xB0, 0xC8, 0x00, 0x10, 0x40, 0x81, 0xFF,
+    0xA1, 0xA6, 0xA8, 0x3F, 0xA4, 0xD3, 0x00, 0xD5, 0xF0, 0xD9,
+    0x22, 0xDA, 0x12, 0xDB, 0x20, 0x8D, 0x14, 0xAF, 0x2E};
+/* An array you can use for the commands to be sent for scrolling. */
+static uint8_t SSD1306_Scroll_Commands[8] = {0};
+/* The following functions are provided for you to use, without requiring any
+ * modifications */
+void SSD1306_UpdateScreen(void) {
+  uint8_t m;
+  uint8_t packet[3] = {0x00, 0x00, 0x10};
+  for (m = 0; m < 8; m++) {
+    packet[0] = (0xB0 + m);
+    ssd1306_I2C_Write(SSD1306_I2C_ADDR, 0x00, packet, 3);
+    ssd1306_I2C_Write(SSD1306_I2C_ADDR, 0x40,
+                      &SSD1306_Buffer[SSD1306_WIDTH * m], SSD1306_WIDTH);
+  }
+}
+void SSD1306_Fill(SSD1306_COLOR_t color) {
+  /* Use memset to efficiently set the entire SSD1306_Buffer to a single value.
+   */
+  memset(SSD1306_Buffer, (color == SSD1306_COLOR_BLACK) ? 0x00 : 0xFF,
+         sizeof(SSD1306_Buffer));
+}
+void SSD1306_Clear(void) {
+  SSD1306_Fill(0);
+  SSD1306_UpdateScreen();
+}
+/* Start of the functions you must complete for this lab. */
+#define SSD1306_TXBUF_MAX (1 + SSD1306_WIDTH)  // 129 if width = 128
+static uint8_t tx_buf[SSD1306_TXBUF_MAX];
+void ssd1306_I2C_Write(uint8_t address, uint8_t reg, uint8_t* data,
+                       uint16_t count) {
+  /* TODO */
+  // Safety: clamp count so we don't overflow tx_buf
+  if (count > (SSD1306_TXBUF_MAX - 1)) {
+    count = SSD1306_TXBUF_MAX - 1;
+  }
+  tx_buf[0] = reg;  // control byte
+  // copy count bytes of payload
+  for (uint16_t i = 0; i < count; i++) {
+    tx_buf[i + 1] = data[i];
+  }
+  HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(&hi2c2, address, tx_buf,
+                                                     count + 1, HAL_MAX_DELAY);
+  if (status == HAL_OK) {
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);    // Green LED on
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);  // Error LED off
+  } else {
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);  // Green LED off
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);    // Error LED on
+  }
+}
+HAL_StatusTypeDef SSD1306_Init(void) {
+  /* Check if the OLED is connected to I2C */
+  if (HAL_I2C_IsDeviceReady(&hi2c2, SSD1306_I2C_ADDR, 1, 20000) != HAL_OK) {
+    return HAL_ERROR;
+  }
+  /* Keep this delay to prevent overflowing the I2C controller */
+  HAL_Delay(10);
+  /* Init LCD */
+  for (uint16_t i = 0; i < sizeof(SSD1306_Init_Config); i++) {
+    uint8_t cmd = SSD1306_Init_Config[i];
+    // control byte 0x00 = command path
+    ssd1306_I2C_Write(SSD1306_I2C_ADDR, 0x00, &cmd, 1);
+  }
+  /* Clear screen */
+  SSD1306_Fill(SSD1306_COLOR_BLACK);
+  /* Update screen */
+  SSD1306_UpdateScreen();
+  /* Return OK */
+  return HAL_OK;
+}
+HAL_StatusTypeDef SSD1306_SetPixel(uint16_t x, uint16_t y,
+                                   SSD1306_COLOR_t color) {
+  if (x >= SSD1306_WIDTH || y >= SSD1306_HEIGHT) {
+    return HAL_ERROR;
+  }
+  uint16_t page = y / 8;
+  uint16_t index = x + (page * SSD1306_WIDTH);
+  uint8_t bit = y % 8;
+  /* Set the pixel at position (x,y) to 'color'. */
+  if (color == SSD1306_COLOR_WHITE) {
+    /* TODO */
+    SSD1306_Buffer[index] |= (1 << bit);
+  } else {
+    SSD1306_Buffer[index] &= ~(1 << bit);
+  }
+  return HAL_OK;
+}
+void SSD1306_Scroll(SSD1306_SCROLL_DIR_t direction, uint8_t start_row,
+                    uint8_t end_row) {
+  /* TO DO */
+  // Pages are 0..7 on a 64-pixel-tall SSD1306 (8 pages total)
+  if (start_row > 7) start_row = 7;
+  if (end_row > 7) end_row = 7;
+  if (start_row > end_row) {
+    uint8_t tmp = start_row;
+    start_row = end_row;
+    end_row = tmp;
+  }
+  // Optional but recommended: stop any existing scroll first
+  //	    {
+  //	        uint8_t stop = SSD1306_DEACTIVATE_SCROLL;
+  //	        ssd1306_I2C_Write(SSD1306_I2C_ADDR, 0x00, &stop, 1);
+  //	    }
+  SSD1306_Scroll_Commands[0] = (direction == SSD1306_SCROLL_RIGHT)
+                                   ? SSD1306_RIGHT_HORIZONTAL_SCROLL
+                                   : SSD1306_LEFT_HORIZONTAL_SCROLL;
+  SSD1306_Scroll_Commands[1] = 0x00;       // dummy
+  SSD1306_Scroll_Commands[2] = start_row;  // first page
+  SSD1306_Scroll_Commands[3] = 0x00;       // speed (default)
+  SSD1306_Scroll_Commands[4] = end_row;    // last page
+  SSD1306_Scroll_Commands[5] = 0x00;       // dummy
+  SSD1306_Scroll_Commands[6] = 0xFF;       // dummy
+  SSD1306_Scroll_Commands[7] = 0x2F;       // 0x2F
+  // Send all 8 bytes as commands
+  ssd1306_I2C_Write(SSD1306_I2C_ADDR, 0x00, SSD1306_Scroll_Commands, 8);
+}
+void SSD1306_Stopscroll(void) {
+  /* TO DO */
+  uint8_t cmd = 0x2E;  // Deactivate scroll
+  ssd1306_I2C_Write(SSD1306_I2C_ADDR, 0x00, &cmd, 1);
+}
+void SSD1306_Putc(uint16_t x, uint16_t y, char ch, FontDef_t* Font) {
+  if (x >= SSD1306_WIDTH || y >= SSD1306_HEIGHT)
+    return;  // This is lowk not necessary but making sure x and y are within
+             // bounds of the screen
+  // ASCII printable characters are from 32 to 126 so have to check if the
+  // character is valid or not
+  if (ch < 32 || ch > 126) {
+    ch = '?';  // fallback because its an invalid character (So if its like an
+               // invalid character it falls back to '?'
+    if (ch < 32 || ch > 126) return;
+  }
+  // Each character = Font->FontHeight rows.
+  // For Font11x18: height=18, and each row is a uint16_t.
+  // Typical layout: data is packed as [char0 row0..rowH-1, char1 row0.., ...]
+  const uint16_t* char_data =
+      (const uint16_t*)&Font
+          ->data[(ch - 32) * Font->FontHeight];  // We need to find the bitmap
+                                                 // of this character
+  for (uint16_t row = 0; row < Font->FontHeight;
+       row++) {  // For 11x18 it goes from 0 to 18
+    uint16_t bits = char_data[row];
+    // Only the most significant Font->FontWidth bits are valid.
+    // For Font11x18, width=11: check bits 15 down to 5.
+    for (uint16_t col = 0; col < Font->FontWidth;
+         col++) {  // Col = 0 is left most pixel and col = 10 is right most
+                   // pixel for 11x18
+      // MSB-first: col=0 tests bit15, col=10 tests bit(15-10)=5
+      uint16_t mask = (uint16_t)(1U << (15 - col));
+      if (bits & mask) {
+        SSD1306_SetPixel(x + col, y + row, SSD1306_COLOR_WHITE);
+      } else {
+        // Optional: draw background as black
+        SSD1306_SetPixel(x + col, y + row, SSD1306_COLOR_BLACK);
+      }
+    }
+  }
+}
+HAL_StatusTypeDef SSD1306_Puts(char* str, FontDef_t* Font) {
+  // Static cursor survives between calls
+  static uint16_t cx = 0;
+  static uint16_t cy = 0;
+  while (*str != '\0') {
+    // Handle newline
+    if (*str == '\n') {  // For new line
+      cx = 0;
+      cy += Font->FontHeight;
+      str++;
+      continue;
+    }
+    // Wrap to next line if we hit right edge
+    if (cx + Font->FontWidth >= SSD1306_WIDTH) {
+      cx = 0;
+      cy += Font->FontHeight;
+    }
+    // Stop if we run out of vertical space
+    if (cy + Font->FontHeight >= SSD1306_HEIGHT) {
+      return HAL_ERROR;
+    }
+    // Draw character
+    SSD1306_Putc(cx, cy, *str, Font);
+    // Advance cursor horizontally
+    cx += Font->FontWidth;
+    str++;
+  }
+  return HAL_OK;
+}
+
+void OLED_DrawRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+    for (uint16_t i = x; i < x + w; i++)
+    {
+        SSD1306_SetPixel(i, y, SSD1306_COLOR_WHITE);
+        SSD1306_SetPixel(i, y + h - 1, SSD1306_COLOR_WHITE);
+    }
+
+    for (uint16_t j = y; j < y + h; j++)
+    {
+        SSD1306_SetPixel(x, j, SSD1306_COLOR_WHITE);
+        SSD1306_SetPixel(x + w - 1, j, SSD1306_COLOR_WHITE);
+    }
+}
+
+void OLED_FillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+    for (uint16_t j = y; j < y + h; j++)
+    {
+        for (uint16_t i = x; i < x + w; i++)
+        {
+            SSD1306_SetPixel(i, j, SSD1306_COLOR_WHITE);
+        }
+    }
+}
+
+void OLED_DrawText(uint16_t x, uint16_t y, char *str, FontDef_t *font)
+{
+    while (*str)
+    {
+        SSD1306_Putc(x, y, *str, font);
+        x += font->FontWidth;
+        str++;
+    }
+}
+
+void OLED_ShowProgress(uint8_t percent)
+{
+    char buf[8];
+
+    uint16_t bar_x = 14;
+    uint16_t bar_y = 30;
+    uint16_t bar_w = 100;
+    uint16_t bar_h = 12;
+
+    if (percent > 100) percent = 100;
+
+    uint16_t fill_w = ((bar_w - 2) * percent) / 100;
+
+    SSD1306_Fill(SSD1306_COLOR_BLACK);
+
+    // Title
+    OLED_DrawText(15, 8, "Printing ...", &Font_7x10);
+
+    // Bar outline
+    OLED_DrawRect(bar_x, bar_y, bar_w, bar_h);
+
+    // Bar fill
+    if (fill_w > 0)
+    {
+        OLED_FillRect(bar_x + 1, bar_y + 1, fill_w, bar_h - 2);
+    }
+
+    // Percentage text
+    sprintf(buf, "%d%%", percent);
+    OLED_DrawText(45, 48, buf, &Font_7x10);
+
+    SSD1306_UpdateScreen();
+}
